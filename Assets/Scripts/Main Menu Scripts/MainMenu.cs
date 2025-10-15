5832820cbc7b556027d32556ca571d2fcf5ae0d0
Jefferson Garcia <jefferson.garcia3435@gmail.com>
2025-10-15 16:17:57 +0800

    Some Changes

--- Assets/Scripts/Main Menu Scripts/MainMenu.cs
+++ Assets/Scripts/Main Menu Scripts/MainMenu.cs
@@ -6,7 +6,16 @@ public class MainMenu : MonoBehaviour
     public GameObject startGamePanel;
     public GameObject howToPlayPanel;
     public GameObject settingsPanel;
-    
+
+    void Start()
+    {
+        // Only the main menu is active at start
+        mainMenuPanel.SetActive(true);
+        startGamePanel.SetActive(false);
+        howToPlayPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+
 
     public void OpenPanel(GameObject panelToOpen)
     {
@@ -20,4 +29,16 @@ public class MainMenu : MonoBehaviour
         // Open the selected one
         panelToOpen.SetActive(true);
     }
+
+    public void BackToMainMenu()
+    {
+        // Close all panels first
+        startGamePanel.SetActive(false);
+        howToPlayPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+
+        // Open the main menu
+        mainMenuPanel.SetActive(true);
+    }
+
 }
